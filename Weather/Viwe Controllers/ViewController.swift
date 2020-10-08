2c16d9c200a10722e686bdb86a15a21981b0e66a
Alexey <ananin.a.a@mail.ru>
2020-10-08 23:38:20 +0300

    Project preparation

---
+++ Weather/Viwe Controllers/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Alexey on 08.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var weatherIconImageView: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var feelsLikeTemperatureLabel: UILabel!
+
+
+    @IBAction func searchPressed(_ sender: UIButton) {
+        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
